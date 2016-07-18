27e47dedf8c0d5c8bc6e7e19b7c752e48931afbe
Sébastien BEAU <sebastien.beau@akretion.com>
2016-07-18 10:53:56 +0200

    [FIX] fix getting email

--- connector_devise/controllers/main.py
+++ connector_devise/controllers/main.py
@@ -8,7 +8,6 @@ class DeviseSession(Session):
     @http.route('/web/session/authenticate', type='json', auth="none")
     def authenticate(self, db, login, password, base_location=None):
         if isinstance(base_location, dict):
-            base_location = base_location.get('base_location')
             request.session['web_email'] = base_location.get('email')
         return super(DeviseSession, self).authenticate(db, login, password,
                                                        base_location)
